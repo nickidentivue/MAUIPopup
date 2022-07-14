bdea7b96d185c1e92285ba077a7ea56098bf0e44
1888games <nick@1888games.co.uk>
2022-07-14 12:25:39 +0100

    Upload

---
+++ PopupTest/PopupTest/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace PopupTest;
+
+using CommunityToolkit.Maui.Views;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+
+        var popup = new Popup
+        {
+            Content = new VerticalStackLayout
+            {
+                Children =
+        {
+            new Label
+            {
+                Text = "This is a very important message!"
+            }
+        }
+            }
+        };
+
+        this.ShowPopup(popup);
+
+		;
+	}
+
+	
+}
+
